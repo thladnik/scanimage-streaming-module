419770f05fab295188a41f98cf37c487713b5fc9
dburkhardt <103647860+DavidB42@users.noreply.github.com>
2025-01-16 13:10:53 +0100

    Update USRFC_SSM_InitTcpConnEnv.m

--- USRFC_SSM_InitTcpConnEnv.m
+++ USRFC_SSM_InitTcpConnEnv.m
@@ -3,9 +3,9 @@ function USRFC_SSM_InitTcpConnEnv(source, event, arguments)
     global SSM_TcpClient;
 
     % load test image
-    %t = Tiff('test_00001.tif','r');
+    t = Tiff('test_00001.tif','r');
 
-    setup the tcp client
+    %setup the tcp client
     SSM_TcpClient = struct;
     SSM_TcpClient.test_tiff_image= read(t);
     SSM_TcpClient.connection_status = false;
